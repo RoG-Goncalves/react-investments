4724cb269d1fbd7ed33d4deaf971fc1bd0ee205a
rog-goncalves <rodrigo.oliveira.goncalves@gmail.com>
2021-08-23 10:50:37 -0300

    adjusted investment.jsx

--- src/components/Investment.jsx
+++ src/components/Investment.jsx
@@ -1,22 +1,23 @@
 import TotalRevenues from "./TotalRevenues";
 
 export default function Investment({ children: investment }) {
+  const { reports } = investment;
   return (
     <div key={investment.id} className="m-4 border p-2">
       <h2 className="text-center font-semibold text-lg">
         {investment.description}
       </h2>
       <span className="text-center">
-        <TotalRevenues>{investment.reports}</TotalRevenues>
+        <TotalRevenues>{reports}</TotalRevenues>
       </span>
-      {investment.reports
-        .sort((a, b) => {
-          return a.month - b.month;
-        })
+      {
+        //prettier-ignore
+        reports
+        .sort((a, b) => {return a.month - b.month;})
         .map((report, index) => {
           const { monthName, year, value } = report;
           //prettier-ignore
-          const previousValue = investment.reports[index > 0 ? index - 1 : index].value;
+          const previousValue = reports[index > 0 ? index - 1 : index].value;
           const monthlyRevenue = Math.round((value / previousValue - 1) * 100);
           //prettier-ignore
           const checkClass = monthlyRevenue > 0 ? "text-green-800" : "text-red-800 font-semibold";
@@ -36,7 +37,8 @@ export default function Investment({ children: investment }) {
               <div className={`text-right`}>{monthlyRevenue} %</div>
             </div>
           );
-        })}
+        })
+      }
     </div>
   );
 }
